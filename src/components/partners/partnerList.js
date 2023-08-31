db471d3a1470d994b7d4972ebd916e5cd8c447d0
Pedro Pereira <plp.leoncio@gmail.com>
2023-08-31 18:05:13 -0300

    feat: imagens de parceiros

---
+++ src/components/partners/partnerList.js
@@ -0,0 +1,69 @@
+import cimentoNacional from "../../assets/images/logos/cimento-nacional.png"
+import deltaPorcelanatos from "../../assets/images/logos/delta-porcelanatos.png"
+import embramaco from "../../assets/images/logos/embramaco.png"
+import fiven from "../../assets/images/logos/fiven.png"
+import gerdau from "../../assets/images/logos/gerdau.png"
+import intercement from "../../assets/images/logos/intercement.png"
+import jundu from "../../assets/images/logos/jundu.png"
+import metalCore from "../../assets/images/logos/metal-core.png"
+import mix from "../../assets/images/logos/mix.png"
+import nadir from "../../assets/images/logos/nadir.png"
+import nexus from "../../assets/images/logos/nexus.png"
+import owens from "../../assets/images/logos/owens.png"
+import usiminas from "../../assets/images/logos/usiminas2.png"
+import vale from "../../assets/images/logos/vale.png"
+import villagres from "../../assets/images/logos/villagres.png"
+import wheaton from "../../assets/images/logos/wheaton.png"
+
+const PARTNER_LIST = [
+    {
+        imageUrl: cimentoNacional
+    },
+    {
+        imageUrl: deltaPorcelanatos
+    },
+    {
+        imageUrl: embramaco
+    },
+    {
+        imageUrl: fiven
+    },
+    {
+        imageUrl: gerdau
+    },
+    {
+        imageUrl: intercement
+    },
+    {
+        imageUrl: jundu
+    },
+    {
+        imageUrl: metalCore
+    },
+    {
+        imageUrl: mix
+    },
+    {
+        imageUrl: nadir
+    },
+    {
+        imageUrl: nexus
+    },
+    {
+        imageUrl: owens
+    },
+    {
+        imageUrl: usiminas
+    },
+    {
+        imageUrl: vale
+    },
+    {
+        imageUrl: villagres
+    },
+    {
+        imageUrl: wheaton
+    },
+]
+
+export default PARTNER_LIST
